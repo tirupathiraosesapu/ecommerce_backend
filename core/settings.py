6e2c88fd25739d310a60b05b276bcefc3a32d224
tirupathiraosesapu <tirupathiraosesapu@gmail.com>
2026-09-22 12:47:53 +0530

    Configure strict MySQL database connection

--- core/settings.py
+++ core/settings.py
@@ -74,9 +74,9 @@ DB_HOST = os.getenv('DB_HOST')
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME', 'ecommerce_db'),
-            'USER': os.getenv('DB_USER', 'root'),
-            'PASSWORD': os.getenv('DB_PASSWORD', ''),
+            'NAME': os.getenv('DB_NAME', 'u859141856_ecommerce_db2'),
+            'USER': os.getenv('DB_USER', 'u859141856_tirupathi_new'),
+            'PASSWORD': os.getenv('DB_PASSWORD', 'VKkb@h494rJ6S8$'),
             'HOST': DB_HOST,
             'PORT': os.getenv('DB_PORT', '3306'),
             'OPTIONS': {
